74bf68e8179f069217570782a3178d8f48be6b2d
halouworls <halouworls@gmail.com>
2020-09-29 04:18:57 +0800

    switch debug env to bochs, for better debug experience

--- BootLoader/BuildBootBin.bat
+++ BootLoader/BuildBootBin.bat
@@ -10,6 +10,13 @@ pause > NUL
 ECHO "按任意键启动Qemu运行BootSector"
 pause > NUL
 del /Q ..\bin\BootImg.img
-copy  /B /Y ..\bin\Boot-0-Sector.bin + ..\bin\Boot-1-Loader.bin + ..\bin\Boot-2-Kernel.bin+ ..\tool\objcopy.exe ..\bin\BootImg.img
-C:\qemu\qemu-system-x86_64.exe  -drive format=raw,file=..\bin\BootImg.img
+..\tool\dd if=/dev/zero of=..\bin\BootImg.img bs=1M count=64
+..\tool\dd if=..\bin\Boot-0-Sector.bin of=..\bin\BootImg.img
+..\tool\dd if=..\bin\Boot-1-Loader.bin of=..\bin\BootImg.img bs=512 seek=1
+..\tool\dd if=..\bin\Boot-2-Kernel.bin of=..\bin\BootImg.img bs=512 seek=8
+rem copy  /B /Y ..\bin\Boot-0-Sector.bin + ..\bin\Boot-1-Loader.bin + ..\bin\Boot-2-Kernel.bin+ ..\tool\objcopy.exe ..\bin\BootImg.img
+rem C:\qemu\qemu-system-i386.exe  -drive format=raw,file=..\bin\BootImg.img
+
+..\tool\Bochs-2.6.11\bochsdbg.exe  -rc bochsdbg.txt -f bochsrc.bxrc -q -unlock
+pause
 goto BUILD
---
+++ Tool/Bochs-2.6.11/dlxlinux/run.bat
@@ -0,0 +1,2 @@
+cd "C:\Program Files\Bochs-2.6.11\dlxlinux"
+..\bochs -q -f bochsrc.bxrc
